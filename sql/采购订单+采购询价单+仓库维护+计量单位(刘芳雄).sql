ed760f32dd4671e2a6ad3abd22b67fc73e5ee999
yantaoshi <yantaoshi@qq.com>
2018-11-30 09:39:42 +0800

    1111

--- sql/采购订单+采购询价单+仓库维护+计量单位(刘芳雄).sql
+++ sql/采购订单+采购询价单+仓库维护+计量单位(刘芳雄).sql
@@ -96,3 +96,54 @@ select * from  buyxunxi;
 select * from buyOrderxi;
 select * from buyorder;
 select * from wareHouse;
+
+
+
+
+
+insert into unit(unitID,unitName,remark)values('01','PCS','个');
+insert into unit(unitID,unitName,remark)values('02','t','吨');
+insert into unit(unitID,unitName,remark)values('03','m','米');
+insert into unit(unitID,unitName,remark)values('04','cm','厘米');
+select * from unit ;
+
+insert into warehouse(wareID,wareName,contentPer,contentPhone,contentAdre,remark)
+values('01','原料仓库','王某某','15869893674','株洲大桥','很好的仓库');
+insert into warehouse(wareID,wareName,contentPer,contentPhone,contentAdre,remark)
+values('02','成品仓','刘某某','15869893674','株洲大桥河西','很好的仓库');
+select * from warehouse;
+
+insert into buyxun(BillNO,BilDate,CustomerID,ValidDate,PriceofTax,CurrID,Maker,Permitter,SalesID,AStatus)
+values('2018112701',to_date('20181127','yyyymmdd'),'SHJJ',to_date('20181128','yyyymmdd'),1,'RMB','admin','admin','10018',0);
+insert into buyxun(BillNO,BilDate,CustomerID,ValidDate,PriceofTax,CurrID,Maker,Permitter,SalesID,AStatus)
+values('2018112702',to_date('20181127','yyyymmdd'),'SHJJ',to_date('20181128','yyyymmdd'),1,'RMB','admin','admin','10011',0);
+insert into buyxun(BillNO,BilDate,CustomerID,ValidDate,PriceofTax,CurrID,Maker,Permitter,SalesID,AStatus)
+values('2018112703',to_date('20181127','yyyymmdd'),'SHJJ',to_date('20181128','yyyymmdd'),1,'RMB','admin','admin','10019',0);
+select * from buyxun;
+
+insert into buyxunxi(BillNO,BillDate,SerNo,ProdID,ProdClassID,UnitName,ProdName,sQuantity,OldPrice,Discount,sPrice,Amount,TaxRate,TaxAmt,VFToal,SourceTranFlag,SourceTranBi)
+values('2018112701',to_date('20181127','yyyymmdd'),1,'C01-W-HP-DC7900-307','主机','PCS','HP AB2台式机',50,3370.00,100.00,3370.00,3370.00,17.00,572.90,3942.90,'销售订单','2018111701');
+insert into buyxunxi(BillNO,BillDate,SerNo,ProdID,ProdClassID,UnitName,ProdName,sQuantity,OldPrice,Discount,sPrice,Amount,TaxRate,TaxAmt,VFToal,SourceTranFlag,SourceTranBi)
+values('2018112702',to_date('20181127','yyyymmdd'),1,'C01-W-HP-DC7900-308','主机','PCS','HP AB3台式机',50,3370.00,100.00,3370.00,3370.00,17.00,572.90,3942.90,'采购请购单','2018111702');
+insert into buyxunxi(BillNO,BillDate,SerNo,ProdID,ProdClassID,UnitName,ProdName,sQuantity,OldPrice,Discount,sPrice,Amount,TaxRate,TaxAmt,VFToal,SourceTranFlag,SourceTranBi)
+values('2018112703',to_date('20181127','yyyymmdd'),1,'C01-W-HP-DC7900-309','主机','PCS','HP AB4台式机',50,3370.00,100.00,3370.00,3370.00,17.00,572.90,3942.90,'采购询价单','2018111703');
+select * from buyxunxi;
+
+insert into Buyorder(BillNO,BillDate,BillStatus,CustomerID,AddressID,PriceOfTax,SalesName,DepartID,Maker,Permitter,AStatus)values
+('2018112901',to_date('20181129','yyyymmdd'),0,'HP','北大禽鸟',0,'10018','09','admin','admin',0);
+insert into Buyorder(BillNO,BillDate,BillStatus,CustomerID,AddressID,PriceOfTax,SalesName,DepartID,Maker,Permitter,AStatus)values
+('2018112902',to_date('20181129','yyyymmdd'),1,'HP','株洲',0,'10019','02','admin','admin',1);
+insert into Buyorder(BillNO,BillDate,BillStatus,CustomerID,AddressID,PriceOfTax,SalesName,DepartID,Maker,Permitter,AStatus)values
+('2018112903',to_date('20181129','yyyymmdd'),0,'HP','啦啦',0,'10010','01','admin','admin',0);
+select * from Buyorder;
+
+insert into BuyOrderxi(SerNo,ProdID,ProdClassID,ProdClassName,UnitName,sQuantity,OldPrice,Discount,sPrice,Amount,TaxRate,TaxAmt,VFTotal,PreInDate,QtyRemain,TranType,FromNO)
+values(1,'C01-7900-307','001','HP 307PA#AB2台式机','pcs',50,3370.00,100.00,3370.00,3370.0,17.00,572.90,3370.00,to_date('20181129','yyyymmdd'),40,'销售订单','2018111701');
+insert into BuyOrderxi(SerNo,ProdID,ProdClassID,ProdClassName,UnitName,sQuantity,OldPrice,Discount,sPrice,Amount,TaxRate,TaxAmt,VFTotal,PreInDate,QtyRemain,TranType,FromNO)
+values(2,'C01-7900-308','002','HP 307PA#AB2台式机','pcs',50,3370.00,100.00,3370.00,3370.0,17.00,572.90,3370.00,to_date('20181129','yyyymmdd'),40,'销售订单','2018111701');
+
+insert into BuyOrderxi(SerNo,ProdID,ProdClassID,ProdClassName,UnitName,sQuantity,OldPrice,Discount,sPrice,Amount,TaxRate,TaxAmt,VFTotal,PreInDate,QtyRemain,TranType,FromNO)
+values(3,'C01-7900-309','003','HP 307PA#AB2台式机','pcs',50,3370.00,100.00,3370.00,3370.0,17.00,572.90,3370.00,to_date('20181129','yyyymmdd'),40,'销售订单','2018111701');
+select * from BuyOrderxi;
+
+
